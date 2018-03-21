6113881b36e53dcc6b4106366403bd7ba15bbe1e
Bogdan Opanchuk <bogdan@opanchuk.net>
2018-03-21 20:28:54 +1100

    Remove per-element random number generation, which was used for debugging

--- src/numeric-functions.jl
+++ src/numeric-functions.jl
@@ -2,17 +2,6 @@ const Torus32 = Int32
 
 
 function rand_uniform_int32(rng::AbstractRNG, dims...)
-    #=
-    if length(dims) == 0
-        rand(rng, Int32(0):Int32(1))
-    else
-        result = Array{Int32}(dims...)
-        for i in 1:length(result)
-            result[i] = rand(rng, Int32(0):Int32(1))
-        end
-        result
-    end
-    =#
     rand(rng, Int32(0):Int32(1), dims...)
 end
 
@@ -21,34 +10,11 @@ function rand_uniform_torus32(rng::AbstractRNG, dims...)
     # TODO: if dims == () (it happens), the return value is not an array -> type instability
     #       also, there's probably instability for arrays of different dims too.
     #       Naturally, it applies for all other rand_ functions.
-    # TODO: compatibility with the reference, remove later
-
-    if length(dims) == 0
-        rand(rng, Torus32)
-    else
-        result = Array{Torus32}(dims...)
-        for i in 1:length(result)
-            result[i] = rand(rng, Torus32)
-        end
-        result
-    end
-
-    #rand(rng, Torus32, dims...)
+    rand(rng, Torus32, dims...)
 end
 
 
 function rand_gaussian_float(rng::AbstractRNG, sigma::Float64, dims...)
-    #=
-    if length(dims) == 0
-        randn(rng) * sigma
-    else
-        result = Array{Float64}(dims...)
-        for i in 1:length(result)
-            result[i] = rand(rng)
-        end
-        result * sigma
-    end
-    =#
     randn(rng, dims...) * sigma
 end
 
@@ -56,20 +22,8 @@ end
 # Gaussian sample centered in message, with standard deviation sigma
 function rand_gaussian_torus32(rng::AbstractRNG, message::Torus32, sigma::Float64, dims...)
     # Attention: all the implementation will use the stdev instead of the gaussian fourier param
-
-    if length(dims) == 0
-        err = randn(rng) * sigma
-    else
-        err = Array{Float64}(dims...)
-        for i in 1:length(err)
-            err[i] = randn(rng)
-        end
-        err *= sigma
-    end
+    err = randn(rng, dims...) * sigma
     message + dtot32.(err)
-
-    #err = randn(rng, dims...) * sigma
-    #message + dtot32.(err)
 end
 
 
