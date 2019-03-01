cc6fc7b8652e7b60310995ecbfc2d91e45db4d63
Bogdan Opanchuk <bogdan@opanchuk.net>
2019-03-01 18:47:44 +1100

    multikey: eliminate object mutations

--- examples/multikey.jl
+++ examples/multikey.jl
@@ -16,9 +16,6 @@ import TFHE: forward_transform
 using DarkIntegers
 
 
-new_torus_polynomial(len) = TorusPolynomial(Array{Torus32}(undef, len), true)
-
-
 mktfhe_parameters_2party = SchemeParameters(
     500, 0.012467, # LWE parameters
     1024, 1, # TLWE parameters
@@ -69,10 +66,17 @@ struct PublicKey
 end
 
 
-# TGSW sample expanded for all the parties
-# Since the C matrix returned by RGSW.Expand in the paper is sparse,
-# we are keeping only nonzero elements of it.
-mutable struct MKTGswExpSample
+# TGSW sample expanded for all the parties.
+# The full matrix has size `(parties+1)x(parties+1)` and its elements are
+# `decomp_length`-vectors of `TorusPolynomial`.
+# Since the C matrix returned by RGSW.Expand in the paper is sparse and has repeating elements,
+# we are keeping only nonzero elements of it, namely:
+# To save memory, we are only keeping the distinct elements of it, namely:
+# c0 == C_{1,1},
+# c1 == C_{1,party+1}
+# x == C_{i,1}, i = 2...parties+1
+# y == C_{i,party+1}, i == 2...parties+1
+struct MKTGswExpSample
 
     tgsw_params :: TGswParams
     tlwe_params :: TLweParams
@@ -84,24 +88,29 @@ mutable struct MKTGswExpSample
 
     current_variance :: Float64 # avg variance of the sample
 
-    function MKTGswExpSample(tgsw_params::TGswParams, tlwe_params::TLweParams, parties::Int)
+    function MKTGswExpSample(
+            tgsw_params::TGswParams, tlwe_params::TLweParams,
+            x::Array{TorusPolynomial, 2},
+            y::Array{TorusPolynomial, 2},
+            c0::Array{TorusPolynomial, 1},
+            c1::Array{TorusPolynomial, 1},
+            current_variance::Float64)
 
         decomp_length = tgsw_params.decomp_length
         p_degree = tlwe_params.polynomial_degree
+        parties = size(x, 2)
 
-        x = [new_torus_polynomial(p_degree) for i in 1:decomp_length, j in 1:parties]
-        y = [new_torus_polynomial(p_degree) for i in 1:decomp_length, j in 1:parties]
-        c0 = [new_torus_polynomial(p_degree) for i in 1:decomp_length]
-        c1 = [new_torus_polynomial(p_degree) for i in 1:decomp_length]
-
-        current_variance = 0.0
+        @assert size(x) == (decomp_length, parties)
+        @assert size(y) == (decomp_length, parties)
+        @assert size(c0) == (decomp_length,)
+        @assert size(c1) == (decomp_length,)
 
         new(tgsw_params, tlwe_params, x, y, c0, c1, current_variance)
     end
 end
 
 
-mutable struct MKTransformedTGswExpSample
+struct MKTransformedTGswExpSample
 
     tgsw_params :: TGswParams
     tlwe_params :: TLweParams
@@ -131,7 +140,7 @@ end
 
 
 # Uni-encrypted TGSW sample
-mutable struct MKTGswUESample
+struct MKTGswUESample
 
     tgsw_params :: TGswParams
     tlwe_params :: TLweParams
@@ -145,18 +154,22 @@ mutable struct MKTGswUESample
 
     current_variance :: Float64 # avg variance of the sample
 
-    function MKTGswUESample(tgsw_params::TGswParams, tlwe_params::TLweParams)
+    function MKTGswUESample(
+            tgsw_params::TGswParams, tlwe_params::TLweParams,
+            c0::Array{TorusPolynomial, 1}, c1::Array{TorusPolynomial, 1},
+            d0::Array{TorusPolynomial, 1}, d1::Array{TorusPolynomial, 1},
+            f0::Array{TorusPolynomial, 1}, f1::Array{TorusPolynomial, 1},
+            current_variance::Float64)
+
         decomp_length = tgsw_params.decomp_length
         p_degree = tlwe_params.polynomial_degree
 
-        c0 = [new_torus_polynomial(p_degree) for i in 1:decomp_length]
-        c1 = [new_torus_polynomial(p_degree) for i in 1:decomp_length]
-        d0 = [new_torus_polynomial(p_degree) for i in 1:decomp_length]
-        d1 = [new_torus_polynomial(p_degree) for i in 1:decomp_length]
-        f0 = [new_torus_polynomial(p_degree) for i in 1:decomp_length]
-        f1 = [new_torus_polynomial(p_degree) for i in 1:decomp_length]
-
-        current_variance = 0.0
+        @assert size(c0) == (decomp_length,)
+        @assert size(c1) == (decomp_length,)
+        @assert size(d0) == (decomp_length,)
+        @assert size(d1) == (decomp_length,)
+        @assert size(f0) == (decomp_length,)
+        @assert size(f1) == (decomp_length,)
 
         new(tgsw_params, tlwe_params, c0, c1, d0, d1, f0, f1, current_variance)
     end
@@ -176,56 +189,39 @@ function mk_tgsw_encrypt(
     p_degree = tlwe_params.polynomial_degree
     decomp_length = tgsw_params.decomp_length
 
-    result = MKTGswUESample(tgsw_params, tlwe_params)
-
     # The shared randomness
     r = int_polynomial(rand_uniform_bool(rng, p_degree))
 
     # C = (c0,c1) \in T^2dg, with c0 = s_party*c1 + e_c + m*g
-    for i in 1:decomp_length
-        result.c1[i] = torus_polynomial(rand_uniform_torus32(rng, p_degree))
-
-        # c0 = s_party*c1 + e_c + m*g
-        # TODO: it was just key, not key[1] in the original. Hardcoded mask_size=1? Check!
-        result.c0[i] = (
-            torus_polynomial(rand_gaussian_torus32(rng, Int32(0), alpha, p_degree))
-            + message * tgsw_params.gadget_values[i]
-            + transformed_mul(tlwe_key.key[1], result.c1[i]))
-    end
-
+    c1 = [torus_polynomial(rand_uniform_torus32(rng, p_degree)) for i in 1:decomp_length]
+    # TODO: it was just key, not key[1] in the original. Hardcoded mask_size=1? Check!
+    c0 = [
+            (torus_polynomial(rand_gaussian_torus32(rng, Int32(0), alpha, p_degree))
+            + transformed_mul(tlwe_key.key[1], c1[i])
+            + message * tgsw_params.gadget_values[i])
+            for i in 1:decomp_length]
 
     # D = (d0, d1) = r*[Pkey_party | Pkey_parties] + [E0|E1] + [0|m*g] \in T^2dg
-    for i in 1:decomp_length
-
-        # d1 = r*Pkey_parties[i] + E1 + m*g[i]
-        result.d1[i] = (
-            torus_polynomial(rand_gaussian_torus32(rng, Int32(0), alpha, p_degree))
-            + message * tgsw_params.gadget_values[i]
-            + transformed_mul(r, shared_key.a[i]))
-
-        # d0 = r*Pkey_party[i] + E0
-        result.d0[i] = (
-            torus_polynomial(rand_gaussian_torus32(rng, Int32(0), alpha, p_degree))
+    d1 = [
+            (torus_polynomial(rand_gaussian_torus32(rng, Int32(0), alpha, p_degree))
+            + transformed_mul(r, shared_key.a[i])
+            + message * tgsw_params.gadget_values[i])
+            for i in 1:decomp_length]
+    d0 = [
+            (torus_polynomial(rand_gaussian_torus32(rng, Int32(0), alpha, p_degree))
             + transformed_mul(r, public_key.b[i]))
-    end
-
+            for i in 1:decomp_length]
 
     # F = (f0,f1) \in T^2dg, with f0 = s_party*f1 + e_f + r*g
-    for i in 1:decomp_length
+    f1 = [torus_polynomial(rand_uniform_torus32(rng, p_degree)) for i in 1:decomp_length]
+    # TODO: it was just key, not key[1] in the original. Hardcoded mask_size=1? Check!
+    f0 = [
+            (torus_polynomial(rand_gaussian_torus32(rng, Int32(0), alpha, p_degree))
+            + transformed_mul(tlwe_key.key[1], f1[i])
+            + r * tgsw_params.gadget_values[i])
+            for i in 1:decomp_length]
 
-        result.f1[i] = torus_polynomial(rand_uniform_torus32(rng, p_degree))
-
-        # f0 = s_party*f1 + e_f + r*g
-        # TODO: it was just key, not key[1] in the original. Hardcoded mask_size=1? Check!
-        result.f0[i] = (
-            torus_polynomial(rand_gaussian_torus32(rng, Int32(0), alpha, p_degree))
-            + r * tgsw_params.gadget_values[i]
-            + transformed_mul(tlwe_key.key[1], result.f1[i]))
-    end
-
-    result.current_variance = alpha^2
-
-    result
+    MKTGswUESample(tgsw_params, tlwe_params, c0, c1, d0, d1, f0, f1, alpha^2)
 end
 
 
@@ -238,63 +234,43 @@ function mk_tgsw_expand(sample::MKTGswUESample, party::Int, public_keys::Array{P
     decomp_length = tgsw_params.decomp_length
     parties = length(public_keys)
 
-    result = MKTGswExpSample(tgsw_params, tlwe_params, parties)
+    # Calculate the distinct elements of the expanded matrix C.
+    # See the description of `MKTGswUESample` for details.
 
-    # Initialize: C = (0, ..., d1, ..., 0, c1, d0, ..., d0, ..., d0, c0)
-    for j in 1:decomp_length
-        for i in 1:parties
-            result.y[j,i].coeffs .= 0
-        end
+    zero_p = torus_polynomial(zeros(Torus32, p_degree))
 
-        result.y[j,party].coeffs .= sample.d1[j].coeffs
+    # g^{-1}(b_i[j] - b_party[j]) = [u_0, ...,u_dg-1]
+    decomp = [
+        (i == party ?
+            nothing : # unused
+            decompose(public_keys[i].b[j] - public_keys[party].b[j], tgsw_params)
+            )
+        for j in 1:decomp_length, i in 1:parties
+        ]
 
-        result.c1[j].coeffs .= sample.c1[j].coeffs
+    x = [
+        (sample.d0[j] + (i == party ?
+            zero_p :
+            # add xi[j] = <g^{-1}(b_i[j] - b_party[j]), f0>
+            sum(transformed_mul(decomp[j,i][l], sample.f0[l]) for l in 1:decomp_length)))
+        for j in 1:decomp_length, i in 1:parties
+        ]
 
-        for i in 1:parties
-            result.x[j,i].coeffs .= sample.d0[j].coeffs
-        end
-
-        result.c0[j].coeffs .= sample.c0[j].coeffs
-    end
-
-    X = new_torus_polynomial(p_degree)
-    Y = new_torus_polynomial(p_degree)
-    b_temp = new_torus_polynomial(p_degree)
-    u = [new_torus_polynomial(p_degree) for i in 1:decomp_length]
-
-    for i in 1:parties
-        if i != party
-            for j in 1:decomp_length
-                # b_temp = b_i[j] - b_party[j]
-                b_temp = public_keys[i].b[j] - public_keys[party].b[j]
-                # g^{-1}(b_temp) = [u_0, ...,u_dg-1]
-                u = decompose(b_temp, tgsw_params)
-
-                X.coeffs .= 0
-                Y.coeffs .= 0
-                for l in 1:decomp_length
-                    # X = xi[j] = <g^{-1}(b_temp), f0>
-                    X += transformed_mul(u[l], sample.f0[l])
-                    # Y = yi[j] = <g^{-1}(b_temp), f1>
-                    Y += transformed_mul(u[l], sample.f1[l])
-                end
-
-                # xi = d0 + xi
-                result.x[j,i] += X
-                # yi = 0 + yi
-                result.y[j,i] += Y
-            end
-        end
-    end
+    y = [
+        (i == party ?
+            sample.d1[j] :
+            # add yi[j] = <g^{-1}(b_i[j] - b_party[j]), f1>
+            sum(transformed_mul(decomp[j,i][l], sample.f1[l]) for l in 1:decomp_length)
+            )
+        for j in 1:decomp_length, i in 1:parties
+        ]
 
     # TODO: calculate the current variance correctly
-    result.current_variance = sample.current_variance
-
-    result
+    MKTGswExpSample(tgsw_params, tlwe_params, x, y, sample.c0, sample.c1, 0.0)
 end
 
 
-mutable struct MKLweSample
+struct MKLweSample
 
     params :: LweParams
     a :: Array{Torus32, 2} # masks from all parties: (n, parties)
@@ -455,24 +431,17 @@ function mk_decrypt(secret_keys::Array{SecretKey, 1}, sample::MKLweSample)
 end
 
 
-mutable struct MKTLweSample
+struct MKTLweSample
     params :: TLweParams
     a :: Array{TorusPolynomial, 1} # mask (mask_size = 1, so length = parties)
     b :: TorusPolynomial
     current_variance :: Float64
 
-    function MKTLweSample(params::TLweParams, parties::Int)
-        p_degree = params.polynomial_degree
-
-        # TODO: assumes mask_size=1 here?
-        a = [new_torus_polynomial(p_degree) for i in 1:parties]
-        b = new_torus_polynomial(p_degree)
-
-        current_variance = 0.0
+    function MKTLweSample(
+            params::TLweParams, a::Array{TorusPolynomial, 1}, b::TorusPolynomial,
+            current_variance::Float64)
         new(params, a, b, current_variance)
     end
-
-    MKTLweSample(params, a, b, cv) = new(params, a, b, cv)
 end
 
 
@@ -512,49 +481,41 @@ function mk_tgsw_extern_mul(
     p_degree = tlwe_params.polynomial_degree
     decomp_length = tgsw_params.decomp_length
 
-    result = MKTLweSample(tlwe_params, parties)
-
     dec_a = hcat(decompose.(sample.a, Ref(tgsw_params))...)
     dec_b = decompose(sample.b, tgsw_params)
+
+    # It would be faster to do all the summations in transformed space and then transform back.
+    # But, because of the transform used (FFT), the precision is limited,
+    # and we are close to that limit.
+    # Elements in `dec_a` and `dec_b` use `bs_log2_base` bits.
+    # Elements in `exp_sample` arrays have full range of Int32 (32 bits).
+    # So if we want to do all the calculations in this function in transformed space, that takes
+    # `bs_log2_base + 32 + log2(p_degree) + ceil(log2(decomp_length * (parties+1)))` bits
+    # == 8 + 32 + 10 + ceil(log2(4 * 3)) = 54 bits - not good.
 
     tr_dec_a = forward_transform.(dec_a)
     tr_dec_b = forward_transform.(dec_b)
 
     # c'_i = g^{-1}(a_i)*d1, i<parties, i!=party
-    for i in 1:parties
-        result.a[i].coeffs .= 0
-        if i != party
-            for l in 1:decomp_length
-                result.a[i] += inverse_transform(tr_dec_a[l,i] * exp_sample.y[l,party])
-            end
-        end
-    end
-
     # c'_party = \sum g^{-1}(a_i)*yi + g^{-1}(b)*c1
-    result.a[party].coeffs .= 0
-    for i in 1:parties
-        for l in 1:decomp_length
-            result.a[party] += inverse_transform(tr_dec_a[l,i] * exp_sample.y[l,i])
-        end
-    end
-    for l in 1:decomp_length
-        result.a[party] += inverse_transform(tr_dec_b[l] * exp_sample.c1[l])
-    end
+    a = [
+        (i == party ?
+            (sum([inverse_transform(tr_dec_a[l,j] * exp_sample.y[l,j])
+                for l in 1:decomp_length, j in 1:parties])
+            + sum([inverse_transform(tr_dec_b[l] * exp_sample.c1[l])
+                for l in 1:decomp_length])) :
+            sum([inverse_transform(tr_dec_a[l,i] * exp_sample.y[l,party])
+                for l in 1:decomp_length]))
+        for i in 1:parties]
 
     # c'_parties = \sum g^{-1}(a_i)*xi + g^{-1}(b)*c0
-    result.b.coeffs .= 0
-    for i in 1:parties
-        for l in 1:decomp_length
-            result.b += inverse_transform(tr_dec_a[l,i] * exp_sample.x[l,i])
-        end
-    end
-    for l in 1:decomp_length
-        result.b += inverse_transform(tr_dec_b[l] * exp_sample.c0[l])
-    end
+    b = (sum([inverse_transform(tr_dec_a[l,i] * exp_sample.x[l,i])
+            for l in 1:decomp_length, i in 1:parties])
+        + sum([inverse_transform(tr_dec_b[l] * exp_sample.c0[l])
+            for l in 1:decomp_length]))
 
-    # ORIG_TODO: calculate current_variance
-
-    result
+    # TODO: calculate current_variance
+    MKTLweSample(tlwe_params, a, b, 0.)
 end
 
 
